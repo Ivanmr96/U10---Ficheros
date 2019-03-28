bea237800d5e42f4b7d7b24fae5f21d9baa4470e
Iván Moreno <morenoivan756@gmail.com>
2019-03-28 18:08:12 +0100

    Ejercicios

---
+++ Ejercicios/Utilidades/src/UtilidadesFile.java
@@ -0,0 +1,16 @@
+
+public class UtilidadesFile 
+{
+	/* INTERFAZ
+	 * Comentario: Lista el contenido de un directorio
+	 * Prototipo: public void listarContenido(String ruta)
+	 * Entrada: La ruta del directorio
+	 * Precondiciones: La ruta del directorio debe existir
+	 * Salida: Nada.
+	 * Postcondiciones: Nada. solo imprime en pantalla el contenido del directorio.
+	 */
+	public void listarContenido(String ruta)
+	{
+		System.out.print("En resguardo");
+	}
+}
